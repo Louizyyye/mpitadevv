5b86ae9ff73156d64ede8dfb9f28951d341caa06
Dreteck <andredenmark005@gmail.com>
2025-10-05 03:20:55 +0300

    django packaging

---
+++ app/core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def home(request):
+    """Landing page view"""
+    context = {"title": "Home"}
+    return render(request, "home.html", context)
+
+
+def about(request):
+    """About page"""
+    context = {"title": "About"}
+    return render(request, "about.html", context)
